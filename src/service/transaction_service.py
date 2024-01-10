68061ea9d218cbfe48e53458d1fa1c3a444f90ca
vcjuliocesar <vcjuliocesar@outlook.com>
2024-01-09 23:33:06 -0600

    refactor services

--- src/service/transaction_service.py
+++ src/service/transaction_service.py
@@ -1,5 +1,7 @@
+from fastapi import Depends
 from src.domain.models.transaction_entity import TransactionEntity
 from src.infrastructure.dtos.create_transaction_dto import CreateTransactionDto
+from src.infrastructure.exceptions.account_not_found_exception import AccountNotFoundException
 from src.infrastructure.repositories.transaction_repository import TransactionRepository
 from src.infrastructure.exceptions.transaction_not_found_exception import TransactionNotFoundException
 from src.infrastructure.schemas import transaction_schema
@@ -7,10 +9,10 @@ from src.infrastructure.schemas import transaction_schema
 
 class TransactionService:
 
-    def __init__(self, repository: TransactionRepository) -> None:
+    def __init__(self, repository: TransactionRepository = Depends()) -> None:
 
         self.repository = repository
-
+        
     def create(self, transaction) -> TransactionEntity:
 
         return self.repository.create(TransactionEntity(**transaction.model_dump()))
@@ -18,7 +20,7 @@ class TransactionService:
     def find_by_id(self, transaction_id: str) -> TransactionEntity:
 
 
-        exists = self.find_by({'transaction_id': transaction_id})
+        exists = self.find_one_by({'transaction_id': transaction_id})
 
         if not exists:
 
@@ -34,6 +36,21 @@ class TransactionService:
         
         return self.repository.get()
     
+        
     def delete(self, transaction: transaction_schema) -> None:
 
         return self.repository.delete(transaction)
+
+    def get_income_and_expense(self,account_id):
+        
+        return self.repository.get_income_and_expense(account_id)
+    
+    def get_balance(self,account_id):
+        
+        exists = self.repository.find_one_by_account_id(account_id)
+
+        if not exists:
+            
+            raise AccountNotFoundException()
+        
+        return self.repository.get_balance(account_id)
